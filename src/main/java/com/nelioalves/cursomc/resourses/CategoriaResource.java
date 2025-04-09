83ff1a3be3a00c3dc4a3d1861b72901f751e4c09
AndreGomes010 <andregomes01010@gmail.com>
2025-04-09 17:11:12 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/nelioalves/cursomc/resourses/CategoriaResource.java
+++ src/main/java/com/nelioalves/cursomc/resourses/CategoriaResource.java
@@ -1,9 +1,6 @@
 package com.nelioalves.cursomc.resourses;
 
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +21,7 @@ public class CategoriaResource {
 	
 	@RequestMapping(value="/{id}",method=RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
+		
 		Categoria obj = service.buscar(id);
 		return ResponseEntity.ok(obj);
 		
